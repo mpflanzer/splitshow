5f51e595cc37b9ee61723b0e03de1fc53db72850
gunnar.schaefer <gunnar.schaefer@fe67c2be-bc4d-0410-929b-fd4278b6d6b6>
2008-05-23 14:42:57 +0000

    - added SplitShow icon (dummy) to screenshot - reduced the size of the smaller screenshot (still almost 200K)

--- trunk/doc/screenshot/make_composite.sh
+++ trunk/doc/screenshot/make_composite.sh
@@ -7,4 +7,4 @@ convert -size 1055x710 xc:transparent \
         shot_interleaved.png -geometry +150+250 -composite \
         shot_composite.png
 
-convert shot_composite.png -resize 70% shot_composite_sm.png
+convert shot_composite.png -resize 50% -quality 95 shot_composite_sm.png
