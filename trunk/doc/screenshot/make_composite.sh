ae6ef99a4d881652648f35cf0353009cac6802b6
gunnar.schaefer <gunnar.schaefer@fe67c2be-bc4d-0410-929b-fd4278b6d6b6>
2009-02-22 22:13:04 +0000

    - added an icon to the application - updated the documentation section accordingly

--- trunk/doc/screenshot/make_composite.sh
+++ trunk/doc/screenshot/make_composite.sh
@@ -5,6 +5,6 @@ set -e
 convert -size 1055x710 xc:transparent \
         shot_mirror.png -composite \
         shot_interleaved.png -geometry +150+250 -composite \
-        shot_composite.png
+        -resize 50% -quality 95 shot_composite.png
 
-convert shot_composite.png -resize 50% -quality 95 shot_composite_sm.png
+#convert shot_composite.png -resize 50% -quality 95 shot_composite_sm.png
